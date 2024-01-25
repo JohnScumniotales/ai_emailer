012d436f30dc918a5bb779c4e0cc6ef1023c7cbe
JohnScumniotales <148183329+JohnScumniotales@users.noreply.github.com>
2024-01-24 23:06:44 -0800

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -108,7 +108,7 @@ def Main():
     
     with tabMain:
         
-        st.subheader("This tool utlilizes GPT 3.5 to generate emails for potential sponsors.",subheader='red')
+        st.subheader("This tool utlilizes GPT 3.5 to generate emails for potential sponsors.",divider='red')
         st.text("Created for Global Formula Racing (GFR)")
         
         st.write("")
@@ -152,7 +152,7 @@ def Main():
         st.write("")
         st.write("")
 
-        st.subheader("Models 🤖",subheader='red')
+        st.subheader("Models 🤖",divider='red')
         st.markdown(
             """
             ChatGPT 3.5 and ChatGPT 4 are pinnacle models in conversational AI. With 175 billion  
@@ -167,7 +167,7 @@ def Main():
         st.write("")
         st.write("")
 
-        st.subheader("LLM Temperature 🌡️",subheader='red')
+        st.subheader("LLM Temperature 🌡️",divider='red')
         st.markdown(
             """
             In a Language Model like GPT-3.5, temperature is a crucial setting that controls the randomness of text generation.  
@@ -182,7 +182,7 @@ def Main():
         st.write("")
         st.write("")
 
-        st.subheader("API Key🗝️",subheader='red')
+        st.subheader("API Key🗝️",divider='red')
         st.markdown(
             """
             An OpenAI API key is a secure passkey that enables developers to access and  
